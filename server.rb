d557eca847efa24b10990bb5faee894cff5fa8cb
isabel cusick <cusick.isabel@gmail.com>
2015-12-06 21:24:00 -0500

    added add new article form

--- server.rb
+++ server.rb
@@ -6,13 +6,36 @@ module App
    	erb :index
    end 
 
+   # get "/categories/:id/articles" do
+   # 	id = params[:id]
+   # 	# @category = Category.find(id)
+   # 	@article = Article.where(:category_id => id)
+   # 	erb :list
+   # end
+   
+   get "/articles" do
+   	@articles = Article.all
+   	erb :list
+   end
+   
    get "/episodes" do
    	# find everything in article table with a category of one and print the title
-   	@path = "articles"
+   	# @path = "articles"
    	@articles = Article.where(:category_id => 1)
    	erb :list
    end
 
+   get "/articles/new" do
+   	erb :new
+   end
+
+   # maybe it would be better to have four different "new" erbs so that when you went to the new form there wouldn't be all these sections that you aren't going to fill out. right now i have just the one to cut down on the list of gets and posts
+   
+   post "/articles" do 
+   	Article.create({title: params["title"], bio: params["bio"], antagonist: params["antagonist"], special_powers: params["special_powers"], minions: params["minions"], buffy_quarrel: params["buffy_quarrel"], demise: params["demise"], category_id: params["category_id"]})
+   	redirect to "/"
+   end
+   
    get "/articles/:id" do
    	# print out the title of an episode(which is the title of the article) and then everything from the subtopics table whose article_id corresponds to the article
    	id = params[:id]
@@ -23,44 +46,42 @@ module App
    end
 
    get "/characters" do
-   	@path = "characters"
+   	# @path = "characters"
    	@articles = Article.where(:category_id => 2)
    	erb :list
    end
 
-   get "/characters/new" do
-   	erb :new
-   end
    
-   get "/characters/:id" do
-   	id = params[:id]
-  	@article = Article.find(id)
-   	erb :details
-   end
+   
+   # get "/characters/:id" do
+   # 	id = params[:id]
+  	# @article = Article.find(id)
+   # 	erb :details
+   # end
 
    get "/demons" do 
-   	@path= "demons"
+   	# @path= "demons"
    	@articles = Article.where(:category_id => 3)
    	erb :list
    end
 
-   get "/demons/:id" do
-   	id = params[:id]
-   	@article = Article.find(id)
-   	erb :details
-   end
+   # get "/demons/:id" do
+   # 	id = params[:id]
+   # 	@article = Article.find(id)
+   # 	erb :details
+   # end
 
    get "/apocolypses" do 
-   	@path = "apocolypses"
+   	# @path = "apocolypses"
    	@articles = Article.where(:category_id => 4)
    	erb :list
    end 
 
-   get "/apocolypses/:id" do
-   	id = params[:id]
-   	@article = Article.find(id)
-   	erb :details
-   end
+   # get "/apocolypses/:id" do
+   # 	id = params[:id]
+   # 	@article = Article.find(id)
+   # 	erb :details
+   # end
   
 
 
